31cdba07f56def7f3a73eba0743a1ef55bce1195
ThingEngineer <josh.lee.campbell@gmail.com>
2018-09-19 14:58:31 -0500

    user interface enhancements

--- Controller.php
+++ Controller.php
@@ -1,18 +1,18 @@
 <?php
 
 class Projects extends CI_Controller {
-
+	
+	function __construct()
+	{
+		parent::__construct();
+	}
+	
 	function index()
 	{
-
-	}
-
-	function gate()
-	{
 		$this->load->helper(array('file', 'date'));
-
+		
 		$data = json_decode(read_file('../app/logs/gatestate.data'), TRUE);
-
+		
 		switch ($data['gatestate'])
 		{
 			case 0:
@@ -28,44 +28,63 @@ class Projects extends CI_Controller {
 				$view_data['gatestate'] = 'Closing...';
 				break;
 		}
-
-		$last_opened = json_decode(read_file('../app/logs/projects/gateopened.data'), TRUE);
+		
+		$last_opened = json_decode(read_file('../app/logs/gateopened.data'), TRUE);
 		$view_data['last_opened'] = timespan($last_opened['last_opened'], time()) . ' ago';
-
+		
 		//Load View
 		$t['data'] = $view_data;
 		$this->load->view('gate_view', $t);
 	}
-
+	
 	function save_gate_state()
 	{
 		$this->load->helper('file');
-
+		
 		$data = file_get_contents('php://input');
-	    write_file('../app/logs/projects/gatestate.data', $data);
-
+	    write_file('../app/logs/gatestate.data', $data);
+	
 		$data = json_decode($data, TRUE);
 		if ($data['gatestate'] == 1)
 		{
 			$last_opened = array('last_opened' => time());
-			write_file('../app/logs/projects/gateopened.data', json_encode($last_opened));
+			write_file('../app/logs/gateopened.data', json_encode($last_opened));
 		}
 	}
-
+	
 	function get_gate_state()
 	{
 		$this->load->helper(array('file', 'date'));
 		$this->load->library('ajax');
-
-		$data = json_decode(read_file('../app/logs/projects/gatestate.data'), TRUE);
-
-		$last_opened = json_decode(read_file('../app/logs/projects/gateopened.data'), TRUE);
+		
+		$data = json_decode(read_file('../app/logs/gatestate.data'), TRUE);
+		
+		$last_opened = json_decode(read_file('../app/logs/gateopened.data'), TRUE);
 		$data['last_opened'] = timespan($last_opened['last_opened'], time()) . ' ago';
-
-		$this->ajax->output_ajax($data, 'json', FALSE);		// send json data, don't enforce ajax request
+		
+		$data['device_state'] = json_decode(read_file('../app/logs/devicestate.data'), TRUE);
+		$device_offline_timestamp = json_decode(read_file('../app/logs/device_offline_timestamp.data'), TRUE);
+		$data['device_offline_timestamp'] = ' for ' . timespan($device_offline_timestamp['timestamp'], time());
+		
+		$this->ajax->output_ajax($data, 'json', TRUE);		// send json data, enforce ajax request
 	}
-
+	
+	function save_device_state()
+	{
+		$this->load->helper('file');
+		
+		$data = file_get_contents('php://input');
+	    write_file('../app/logs/devicestate.data', $data);
+	
+		$data = json_decode($data, TRUE);
+		if ($data['devicestate'] == 0)
+		{
+			$device_offline_timestamp = array('timestamp' => time());
+			write_file('../app/logs/device_offline_timestamp.data', json_encode($device_offline_timestamp));
+		}
+	}
+	
 }
 
-/* End of file projects.php */
+/* End of file Projects.php */
 /* Location: ./application/controllers/projects.php */
--- gate_view.php
+++ gate_view.php
@@ -7,7 +7,7 @@
     <meta name="author" content="Josh Campbell">
     <link rel="shortcut icon" href="/bootstrap/assets/ico/favicon.png">
 
-    <title>IoT Gate Opperator Addon</title>
+    <title>IoT Gate Operator Addon</title>
 
     <!-- Bootstrap core CSS -->
     <link href="/bootstrap/css/bootstrap.css" rel="stylesheet">
@@ -22,6 +22,40 @@
       <script src="/bootstrap/assets/js/respond.min.js"></script>
     <![endif]-->
 
+	<style>
+	.button {
+	    position: relative;
+	    border: none;
+	    color: #FFFFFF;
+	    text-align: center;
+	    -webkit-transition-duration: 0.4s; /* Safari */
+	    transition-duration: 0.4s;
+	    text-decoration: none;
+	    overflow: hidden;
+	    cursor: pointer;
+	}
+
+	.button:after {
+	    content: "";
+	    background: #90EE90;
+	    display: block;
+	    position: absolute;
+	    padding-top: 300%;
+	    padding-left: 350%;
+	    margin-left: -40px!important;
+	    margin-top: -120%;
+	    opacity: 0;
+	    transition: all 0.8s
+	}
+
+	.button:active:after {
+	    padding: 0;
+	    margin: 0;
+	    opacity: 1;
+	    transition: 0s
+	}
+	</style>
+	
   </head>
 
   <body>
@@ -29,18 +63,18 @@
     <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <!--<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">-->
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">IoT Gate Opperator Addon</a>
+          <a class="navbar-brand" href="#">IoT Gate Operator Addon</a>
         </div>
         <div class="collapse navbar-collapse">
 <!--
           <ul class="nav navbar-nav">
-            <li class="active"><a href="http://projects.ajillion.com/gate">Home</a></li>
-			<li class="active"><a href="http://projects.ajillion.com/gate">Admin</a></li>
+            <li class="active"><a href="http://ajillion.com/projects">Home</a></li>
+			<li class="active"><a href="http://ajillion.com/projects">Admin</a></li>
           </ul>-->
 
         </div>
@@ -49,15 +83,15 @@
 
     <div class="container">
 
-      <div class="starter-template">
-		<button type="button" class="btn btn-lg btn-success btn-block bigger-button" id="open_gate">Open Gate</button><br>
-		<button type="button" class="btn btn-lg btn-primary btn-block" id="latch30m_gate">Latch Open for 30 min</button><br>
-		<button type="button" class="btn btn-lg btn-primary btn-block" id="latch8h_gate">Latch Open for 8 hours</button><br>
-		<button type="button" class="btn btn-lg btn-warning btn-block" id="close_gate">Close Now</button>
-		<br><br>
+      <div class="text-center" style="padding-top:5px; padding-left:15px; padding-right:15px; ">
+		<button type="button" class="button btn btn-lg btn-success btn-block bigger-button" id="open_gate">Open Gate</button><br>
+		<button type="button" class="button btn btn-lg btn-primary btn-block" id="latch30m_gate">Latch Open for 30 min</button><br>
+		<button type="button" class="button btn btn-lg btn-primary btn-block" id="latch8h_gate">Latch Open for 8 hours</button><br>
+		<button type="button" class="button btn btn-lg btn-warning btn-block" id="close_gate">Close Now</button><br>
 		<div class="jumbotron alert alert-info" role="alert">
-		  <strong>Gate Status: </strong><span id="status"><?=$data['gatestate']?></span><br>
-		  Last opened <span id="last_opened"><?=$data['last_opened']?></span>
+		  <strong>The gate is:</strong> <span id="status"><?=$data['gatestate']?></span> <img class="m-0 p-0" id="status-img" src="/images/dual-ring-loader.gif" alt="..." height="32" width="32"><br>
+		  Last opened <span id="last_opened"><?=$data['last_opened']?></span><br>
+		  Controller Status: <span id="device_status" style="color:green"><?=$data['device_status']?></span>
 		</div>
       </div>
 
@@ -70,47 +104,56 @@
 	<script>
 
 	$(document).ready(function(){
+		$("#status-img").hide();
 		resetStatus();
 	})
-
+	
 	function sendJSON(JSONout){
-		var url = 'https://agent.electricimp.com/YOUR-AGENT-CODE';
+		var url = 'https://agent.electricimp.com/-xWQ-bU_8bJ-';
 		$.post(url, JSONout);
 	}
-
+	
 	$("#open_gate").click(function() {
 		var JSONout = '{"c":"btn","val":{"cmd":"open"}}';
 		sendJSON(JSONout);
-		$("#status").text("Opening...");
+		//$("#status").text("Opening");
+		//$("#status-img").show();
+		resetStatus();
 	});
-
+	
 	$("#latch30m_gate").click(function() {
 		var JSONout = '{"c":"btn","val":{"cmd":"latch30m"}}';
 		sendJSON(JSONout);
-		$("#status").text("Opening...");
+		//$("#status").text("Opening");
+		//$("#status-img").show();
+		resetStatus();
 	});
-
+	
 	$("#latch8h_gate").click(function() {
 		var JSONout = '{"c":"btn","val":{"cmd":"latch8h"}}';
 		sendJSON(JSONout);
-		$("#status").text("Opening...");
+		//$("#status").text("Opening");
+		//$("#status-img").show();
+		resetStatus();
 	});
-
+	
 	$("#close_gate").click(function() {
 		var JSONout = '{"c":"btn","val":{"cmd":"close"}}';
 		sendJSON(JSONout);
-		$("#status").text("Closing...");
+		//$("#status").text("Closing");
+		//$("#status-img").show();
+		resetStatus();
 	});
-
+	
 	function resetStatus() {
 		// Target url
 		var target = 'http://projects.ajillion.com/get_gate_state';
-
+		
 		// Request
 		var data = {
 			agent : 'app'
 		};
-
+		
 		// Send ajax post request
 		$.ajax({
 			url: target,
@@ -122,21 +165,32 @@
 				switch(data.gatestate) {
 				    case 0:
 				        $("#status").text('Closed');
+						$("#status-img").hide();
 				        break;
 				    case 1:
-				        $("#status").text('Opening...');
+				        $("#status").text('Opening');
+						$("#status-img").show();
 				        break;
 					case 2:
 				        $("#status").text('Open');
+						$("#status-img").hide();
 				        break;
 					case 3:
-				        $("#status").text('Closing...');
+				        $("#status").text('Closing');
+						$("#status-img").show();
 				        break;
 				    default:
 				        $("#status").text('Error');
+						$("#status-img").hide();
 				}
-
+				
 				$("#last_opened").text(data.last_opened);
+				
+				var device_status = (data.device_state.devicestate ? "Online" : "Offline");
+				if (data.device_state.devicestate == 0) device_status = device_status + data.device_offline_timestamp;
+				var device_status_color = (data.device_state.devicestate ? "green" : "red");
+				$("#device_status").text(device_status);
+				$('#device_status').attr('style','color:' + device_status_color);
 			},
 			error: function(XMLHttpRequest, textStatus, errorThrown)
 			{
@@ -144,7 +198,7 @@
 				$("#status").text('Server Error');
 			}
 		});
-
+		
 		setTimeout(resetStatus, 3000);
 	 }
 
